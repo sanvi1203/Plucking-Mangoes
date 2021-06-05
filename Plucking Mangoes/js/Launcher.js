4f20fea139c4d2ff702d8d015f3de1fd1212a7a8
sanvi1203 <76777789+sanvi1203@users.noreply.github.com>
2021-06-05 20:55:42 +0530

    Add files via upload

---
+++ Plucking Mangoes/js/Launcher.js
@@ -0,0 +1,32 @@
+class Launcher{
+    constructor(body,anchor){
+        var options = {
+            bodyA: body,
+            anchor: anchor,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.bodyA = body;
+        this.pointB = anchor;
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+    attach(body){
+        this.launcher.bodyA = body;
+    }
+    
+    fly(){
+        this.launcher.bodyA = null;
+    }
+
+    display(){
+        if(this.launcher.bodyA){
+            var pointA = this.bodyA.position;
+            var pointB = this.pointB;
+ 
+                strokeWeight(3);
+                line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
